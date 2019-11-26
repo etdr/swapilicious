444735b3b6c320e6a5e39cec8847a07f53445e33
Eli T. Drumm <etd@dte.li>
2019-11-26 13:48:37 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchParametersComponent } from './search-parameters/search-parameters.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchParametersComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/search-parameters/search-parameters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-parameters',
+  templateUrl: './search-parameters.component.html',
+  styleUrls: ['./search-parameters.component.scss']
+})
+export class SearchParametersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
